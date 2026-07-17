68021692af46dd147a97a9f968ad300bbd9f550b
morshedulmunna <morshedulmunna1@gmail.com>
2026-07-17 15:00:47 +0600

    fix: update CURRENT_VERSION to 1.0.1

--- docs/lib/constants.ts
+++ docs/lib/constants.ts
@@ -1,3 +1,3 @@
-export const CURRENT_VERSION = '1.0.0';
+export const CURRENT_VERSION = '1.0.1';
 export const CURRENT_VERSION_TAG = `v${CURRENT_VERSION}`;
 export const LATEST_VERSION_LABEL = `Latest: ${CURRENT_VERSION_TAG}`;
